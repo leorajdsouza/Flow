b0956a739b57742dea35a2f61bfae5fde4f89756
bux bux <bux429@yopmail.com>
2017-03-05 07:32:48 +0000

    added notification plugin

--- Flow/service/notification.js
+++ Flow/service/notification.js
@@ -1,14 +1,15 @@
 
 app.factory("notification", function ($ionicPlatform) {
     return {
-        show: function () {
+        show: function () { 
+            
             $ionicPlatform.ready(function () {
                 cordova.plugins.notification.local.schedule({
                     id: 1,
                     text: "Water Pump is running.",
                     //sound: isAndroid ? 'file://sound.mp3' : 'file://beep.caf',
                     ongoing: true,
-                    icon: 'file://img/icon.png'
+                    icon: 'file://img/icon.png' 
                 });
             });
         },
